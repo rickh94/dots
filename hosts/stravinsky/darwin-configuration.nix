e4e97c5e9a0e43263d47287478c9e9002b263b02
Rick Henry <rickhenry@rickhenry.dev>
2023-02-27 03:17:01 -0500

    whatamess
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- hosts/stravinsky/darwin-configuration.nix
+++ hosts/stravinsky/darwin-configuration.nix
@@ -95,11 +95,13 @@ in
   echo "setting up ~/Applications/NixApps.."
   mkdir -p ~/Applications
   rm -rf ~/Applications/NixApps
+  mkdir -p ~/Applications/NixApps
   chown ${me} ~/Applications/NixApps
   find ${config.system.build.applications}/Applications -maxdepth 1 -type l | while read f; do
     echo "Linking $f"
-    src=$(/usr/bin/statc -f%Y "$f")
-    /usr/bin/osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${me}/Applications/NixApps/\" to POSIX file \"$src\"";
+    src=$(/usr/bin/stat -f%Y "$f")
+    appname="$(basename "$f")"
+    /usr/bin/osascript -e "tell app \"Finder\" to make alias file at POSIX file \"/Users/${me}/Applications/NixApps/\" to POSIX file \"$src\" with properties {name: \"$appname\"}";
   done
   '';
 
