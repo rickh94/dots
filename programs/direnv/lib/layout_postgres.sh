1caf6ac822c2019e7aa943e95f911f1ebe50a27b
Rick Henry <rickhenry@rickhenry.dev>
2023-03-03 02:45:09 -0500

    fix postgres layout
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- programs/direnv/lib/layout_postgres.sh
+++ programs/direnv/lib/layout_postgres.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 layout_postgres() {
+    echo "Initializing Postgres..."
     export PGDATA="$(direnv_layout_dir)/postgres"
     export PGHOST="$PGDATA"
 
     if [[ ! -d "$PGDATA" ]]; then
         initdb
-        cat >> "$PGDATA/postgresql.conf" <<-EOF
-            listen_addresses = ''
-            unix_socket_directories = '$PGHOST'
-        EOF
+        rm "$PGDATA/postgresql.conf"
+        echo "listen_addresses = ''" >> "$PGDATA/postgresql.conf"
+        echo "unix_socket_directories = '$PGHOST'" >> "$PGDATA/postgresql.conf"
         echo "CREATE DATABASE $USER;" | postgres --single -E postgres
     fi
 }
