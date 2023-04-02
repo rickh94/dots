a9ceba0e267faa5734c18c6ce0b26b661597101e
Rick Henry <rickhenry@rickhenry.dev>
2023-04-02 14:23:26 -0400

    Add touchid sudo auth to stravinsky
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- hosts/stravinsky/darwin-configuration.nix
+++ hosts/stravinsky/darwin-configuration.nix
@@ -124,4 +124,17 @@ in
     };
   };
 
+  system.activationScripts.extraActivation.text = ''
+        #!/usr/bin/env bash
+        # PAM settings
+        echo >&2 "setting up pam..."
+    # Enable sudo Touch ID authentication
+    # script from https://gist.github.com/jmatsushita/5c50ef14b4b96cb24ae5268dab613050
+        if ! greq 'pam_tid.so' /etc/pam.d/sudo > /dev/null; then
+          sed -i "" '2i\
+        auth       sufficient     pam_tid.so
+          ' /etc/pam.d/sudo
+        fi
+  '';
+
 }
