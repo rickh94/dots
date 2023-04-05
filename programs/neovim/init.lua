cfe8dc08d3c60c512925266918b650797b1dc845
Rick Henry <rickhenry@rickhenry.dev>
2023-04-05 00:25:17 -0400

    add nvim lilypond suite
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- programs/neovim/init.lua
+++ programs/neovim/init.lua
@@ -321,6 +321,104 @@ require('lazy').setup({
       require('persistence').setup()
     end,
   },
+  {
+    'martineausimon/nvim-lilypond-suite',
+    dependencies = 'MunifTanjim/nui.nvim',
+    config = function()
+      require('nvls').setup({
+        lilypond = {
+          mappings = {
+            player = "<F3>",
+            compile = "<F5>",
+            open_pdf = "<F6>",
+            switch_buffers = "<A-Space>",
+            insert_version = "<F4>",
+            hyphenation = "<F12>",
+            hyphenation_change_lang = "<F11>",
+            insert_hyphen = "<leader>ih",
+            add_hyphen = "<leader>ah",
+            del_next_hyphen = "<leader>dfh",
+            del_prev_hyphen = "<leader>dFh",
+            del_selected_hyphen = "<leader>dh"
+          },
+          options = {
+            pitches_language = "default",
+            output = "pdf",
+            main_file = "main.ly",
+            main_folder = "%:p:h",
+            include_dir = "$HOME",
+            hyphenation_language = "en_DEFAULT"
+          },
+          highlights = {
+            lilyString = { link = "String" },
+            lilyDynamic = { bold = true },
+            lilyComment = { link = "Comment" },
+            lilyNumber = { link = "Number" },
+            lilyVar = { link = "Tag" },
+            lilyBoolean = { link = "Boolean" },
+            lilySpecial = { bold = true },
+            lilyArgument = { link = "Type" },
+            lilyScheme = { link = "Special" },
+            lilyLyrics = { link = "Special" },
+            lilyMarkup = { bold = true },
+            lilyFunction = { link = "Statement" },
+            lilyArticulation = { link = "PreProc" },
+            lilyContext = { link = "Type" },
+            lilyGrob = { link = "Include" },
+            lilyTranslator = { link = "Type" },
+            lilyPitch = { link = "Function" },
+            lilyChord = {
+              ctermfg = "lightMagenta",
+              fg = "lightMagenta",
+              bold = true
+            },
+          },
+        },
+        latex = {
+          mappings = {
+            compile = "<F5>",
+            open_pdf = "<F6>",
+            lilypond_syntax = "<F3>"
+          },
+          options = {
+            clean_logs = false,
+            main_file = "main.tex",
+            main_folder = "%:p:h",
+            include_dir = nil,
+            lilypond_syntax_au = "BufEnter"
+          },
+        },
+        player = {
+          mappings = {
+            quit = "q",
+            play_pause = "p",
+            loop = "<A-l>",
+            backward = "h",
+            small_backward = "<S-h>",
+            forward = "l",
+            small_forward = "<S-l>",
+            decrease_speed = "j",
+            increase_speed = "k",
+            halve_speed = "<S-j>",
+            double_speed = "<S-k>",
+            mpv_flags = {
+              "--msg-level=cplayer=no,ffmpeg=no",
+              "--loop",
+              "--config-dir=/dev/null"
+            }
+          },
+          options = {
+            row = "2%",
+            col = "99%",
+            width = "37",
+            height = "1",
+            border_style = "single",
+            winhighlight = "Normal:Normal,FloatBorder:Normal"
+          },
+        },
+      })
+    end
+  }
 })
 
 vim.cmd.colorscheme('tokyonight-moon')
@@ -833,6 +931,11 @@ for p, c in pairs(color) do
   })
 end
 
+-- lilypond configuration
+vim.api.nvim_create_autocmd('BufEnter', {
+  command = "syntax sync fromstart",
+  pattern = { '*.ly', '*.ily', '*.tex' }
+})
 
 
 -- vim: se ft=lua sw=2 ts=2 expandtab:
