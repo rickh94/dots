7a7a3b0f795ad9d987275aa46dec7c2d883f428e
Rick Henry <rickhenry@rickhenry.dev>
2023-07-26 23:34:15 -0400

    fix typing in hardware
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- hosts/nixserver/hardware-configuration.nix
+++ hosts/nixserver/hardware-configuration.nix
@@ -64,6 +64,6 @@
   # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
