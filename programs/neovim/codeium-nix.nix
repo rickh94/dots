84ccea3a2fb39e3200ac114fcb3a748bd1baee61
Rick Henry <rickhenry@rickhenry.dev>
2023-11-10 17:49:14 -0500

    ugh
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- programs/neovim/codeium-nix.nix
+++ programs/neovim/codeium-nix.nix
@@ -9,7 +9,9 @@ let
     };
     postInstall =
       ''
-        sed -i "/call mkdir(manager_dir, 'p')/ a\\n  let s:bin = \"${codeium-lsp}/bin/codeium-lsp\"" $target/autoload/codeium/server.vim
+        sed -i "/call mkdir(manager_dir, 'p')/ a\ 
+        let s:bin = \"${codeium-lsp}/bin/codeium-lsp\"
+        " $target/autoload/codeium/server.vim
       '';
   };
 in
