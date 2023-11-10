42f27f0e80f0990d8ec003d03627efbc3c73cb7c
Rick Henry <rickhenry@rickhenry.dev>
2023-11-10 17:30:33 -0500

    urgh
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- programs/neovim/codeium-default.nix
+++ programs/neovim/codeium-default.nix
@@ -4,7 +4,7 @@
     plugins = [
       codeium-vim
     ];
-    extraConfig = /* lua */ ''
+    extraLuaConfig = /* lua */ ''
 
       vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
       vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
--- programs/neovim/codeium-nix.nix
+++ programs/neovim/codeium-nix.nix
@@ -22,7 +22,7 @@ in
     extraPackages = [
       codeium-lsp
     ];
-    extraConfig = /* lua */ ''
+    extraLuaConfig = /* lua */ ''
 
       vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
       vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
