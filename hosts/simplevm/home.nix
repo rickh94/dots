79f840d9628be666931cd443c068b9e7da7bc008
Rick Henry <rickhenry@rickhenry.dev>
2023-11-10 17:28:12 -0500

    typo
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- hosts/simplevm/home.nix
+++ hosts/simplevm/home.nix
@@ -3,7 +3,7 @@
   imports = [
     ../_common/default.nix
     ../_common/linux/default.nix
-    ../programs/neovim/codeium-nix.nix
+    ../../programs/neovim/codeium-nix.nix
     ../../programs/wezterm-linux.nix
   ];
 
