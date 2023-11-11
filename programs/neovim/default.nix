3e9f281e1e738fd15c71c1c777fd9f083201a6a1
Rick Henry <rickhenry@rickhenry.dev>
2023-11-10 19:41:24 -0500

    neovim config
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -132,7 +132,7 @@ in
         }
       })
 
-      require('persistence.nvim').setup( {
+      require('persistence').setup( {
         dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
         options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
         pre_save = nil, -- a function to call before saving the session
