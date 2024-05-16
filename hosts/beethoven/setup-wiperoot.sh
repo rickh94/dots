b13b10486ae21c351f20f15a75a07ab79ec4f0f9
Rick Henry <rickhenry@rickhenry.dev>
2024-05-15 22:12:23 -0400

    add wright nix config
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- hosts/beethoven/setup-wiperoot.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir /persist/{impermanence,passwd}
-mkpasswd > /persist/passwd/rick
-cp -r /etc/nixos /persist/impermenance/etc
-cp /etc/machine-id /persist/impermenance/etc/
-ssh-keygen -A
-cp /etc/ssh/ssh_host_*_key{,.pub} /persist/impermenance/etc/ssh
-
--- hosts/stravinsky/home.nix
+++ hosts/stravinsky/home.nix
@@ -25,5 +25,4 @@
     pkgs.rubik
     pkgs.emacs
   ];
-
 }
