942a48cf7538100351248deed10a0a368e00deed
Rick Henry <rickhenry@rickhenry.dev>
2024-05-29 19:01:28 -0400

    add flying dutchman config
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

---
+++ hosts/flyingdutchman/home.nix
@@ -0,0 +1,25 @@
+{ config
+, lib
+, pkgs
+, inputs
+, ...
+}: {
+  imports = [
+    ../_common/default.nix
+    ../_common/linux/default.nix
+    ../../programs/neovim/basic.nix
+  ];
+  home.username = "daveyjones";
+  home.homeDirectory = "/home/daveyjones";
+
+  home.packages = [
+    pkgs.glibc
+    pkgs.xdotool
+    pkgs.feh
+    pkgs.xorg.xmodmap
+    pkgs.xorg.setxkbmap
+    inputs.codeium.packages.x86_64-linux.codeium-lsp
+  ];
+
+  systemd.user.startServices = true;
+}
