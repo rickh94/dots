7697596b4e769a0d69f387e88056a95bf7d962c1
Rick Henry <rickhenry@rickhenry.dev>
2024-06-01 23:44:57 -0400

    update to nix 24.05
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- flake.nix
+++ flake.nix
@@ -11,13 +11,13 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-23.11";
+      url = "github:nixos/nixpkgs/nixos-24.05";
     };
     /*
       nixos-hardware.url = "github:nixos/nixos-hardware/master";
     */
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
