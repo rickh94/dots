df6cfcd3c7a8acbc2ca504f6d2a422fdbfd0bf7c
Rick Henry <rickhenry@rickhenry.dev>
2024-06-12 01:21:55 -0400

    remove some packages from flyingdutchman
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- hosts/flyingdutchman/home.nix
+++ hosts/flyingdutchman/home.nix
@@ -6,8 +6,12 @@
 }: {
   imports = [
     ../_common/default.nix
-    ../_common/linux/desktop.nix
     ../../programs/neovim/basic.nix
+    ../../programs/git.nix
+    ../../programs/starship.nix
+    ../../programs/tmux.nix
+    ../../programs/bat.nix
+    ../../programs/fish.nix
   ];
   home.username = "daveyjones";
   home.homeDirectory = "/home/daveyjones";
@@ -32,14 +36,62 @@
 
   xdg.enable = true;
 
-  home.packages = [
-    pkgs.glibc
-    pkgs.xdotool
-    pkgs.feh
-    pkgs.xorg.xmodmap
-    pkgs.xorg.setxkbmap
-    inputs.codeium.packages.x86_64-linux.codeium-lsp
-  ];
-
   systemd.user.startServices = true;
+
+  home = {
+    stateVersion = "22.11";
+    sessionVariables = {
+      EDITOR = "nvim";
+      MAKEFLAGS = "-j4";
+      NIXPKGS_ALLOW_UNFREE = 1;
+    };
+
+    shellAliases = { };
+
+    packages = with pkgs; [
+      # basics
+      wget
+      tree
+      # neovim
+      zoxide
+      fish
+      mosh
+      zip
+      unzip
+      htop
+      zstd
+      pass
+      bash
+      zsh
+      pv
+      netcat
+      git-crypt
+      gnupg
+
+      # rust replacements
+      difftastic
+      tealdeer
+      dogdns
+      lfs
+      starship
+      carapace
+      fzf
+      ripgrep-all
+      ripgrep
+      bottom
+      eza
+      fd
+      du-dust
+      bat
+      duf
+    ];
+  };
+
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
 }
