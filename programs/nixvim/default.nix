914b9263a2cb3425596e95c1fcb9fdb03dde09e9
Rick Henry <rickhenry@rickhenry.dev>
2025-06-21 15:21:04 -0400

    update

--- programs/nixvim/default.nix
+++ programs/nixvim/default.nix
@@ -1,6 +1,15 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
   helpers = config.lib.nixvim;
+  setlocal_frompattern = [
+    { pattern = [ "*.py" ]; ts = 4; expandtab = true; }
+    # "*.c" = { ts = 8; expandtab = false; };
+    # "*.ino" = { ts = 8; expandtab = false; };
+    # "*.ly" = { ts = 2; expandtab = false; };
+    # "*.sql" = { ts = 4; expandtab = true; };
+  ];
+
+  setlocal_cmd = ts: et: ''setlocal tabstop = ${ts} shiftwidth = ${ts} '' ++ (if et then "expandtab" else "noexpandtab");
 in
 {
   programs.nixvim = {
@@ -9,7 +18,11 @@ in
       mapleader = " ";
       maplocalleader = " ";
     };
+
+    clipboard.register = "unnamedplus";
     opts = {
+      tabstop = 2;
+      expandtab = true;
       hlsearch = false;
       incsearch = true;
       number = true;
@@ -35,15 +48,63 @@ in
 
     keymaps = [
       { action = "<Nop>"; key = "<Space>"; options = { silent = true; }; mode = [ "n" "v" ]; }
+
       # save
       { action = "<cmd>w<cr>"; key = "<leader>w"; mode = [ "n" ]; }
-      #diagnostics
+
+      # diagnostics
       {
         action = helpers.utils.mkRaw "vim.diagnostic.goto_prev";
         key = "]d";
         mode = [ "n" ];
       }
+      {
+        action = helpers.utils.mkRaw "vim.diagnostic.goto_next";
+        key = "[d";
+        mode = [ "n" ];
+      }
+
+      # rebinds
+      { action = "<C-d>zz"; key = "<C-d>"; mode = [ "n" ]; }
+      { action = "<C-u>zz"; key = "<C-u>"; mode = [ "n" ]; }
+      { action = "nzzzv"; key = "n"; mode = [ "n" ]; }
+      { action = "Nzzzv"; key = "N"; mode = [ "n" ]; }
+
+      # paste over selection without losing register
+      { action = ''"_dP''; key = "<leader>p"; mode = [ "x" ]; }
+
+      # system copy paste
+      { action = ''"+y''; key = "<leader>y"; mode = [ "n" "v" ]; }
+      { action = ''"+Y''; key = "<leader>Y"; mode = [ "n" "v" ]; }
+      { action = ''"+p''; key = "<leader>P"; mode = [ "n" "v" ]; }
+
+      # visual mode keybinds
+      { action = ">gv"; key = ">"; mode = [ "v" ]; }
+      { action = "<gv"; key = "<"; mode = [ "v" ]; }
     ];
+
+    autocmd = [
+
+      {
+        command = "echo 'Entering a C or C++ file'";
+        event = [
+          "BufEnter"
+          "BufWinEnter"
+        ];
+        pattern = [
+          "*.c"
+          "*.h"
+        ];
+      }
+
+    ] // builtins.map
+      (a: {
+        pattern = a.p;
+        event = [ "BufReadPre" "BufRead" ];
+        command = setlocal_cmd a.ts a.expandtab;
+      })
+      setlocal_frompattern;
+
     colorschemes.catppuccin = {
       enable = true;
       settings = {
