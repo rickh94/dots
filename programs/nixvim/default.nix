e71a9f487bebc73b3d801dcad3437c8194011c4f
Rick Henry <rickhenry@rickhenry.dev>
2025-06-21 14:20:23 -0400

    update

--- programs/nixvim/default.nix
+++ programs/nixvim/default.nix
@@ -6,33 +6,29 @@
       mapleader = " ";
       maplocalleader = " ";
     };
-    opts.hlsearch = false;
-    # config = {
-    #   opts = {
-    #     hlsearch = false;
-    #     incsearch = true;
-    #     number = true;
-    #     relativenumber = true;
-    #     mouse = "nv";
-    #     breakindent = true;
-    #     undofile = true;
-    #     ignorecase = true;
-    #     smartcase = true;
-    #     updatetime = 50;
-    #     timeout = true;
-    #     timeoutlen = 300;
-    #     completeopt = "menuone,noselect";
-    #     termguicolors = true;
-    #     swapfile = false;
-    #     backup = false;
-    #     undodir = "vim.fn.stdpath('state') .. '/undodir'";
-    #     smartindent = true;
-    #     scrolloff = 8;
-    #     wrap = false;
-    #     cursorline = true;
-    #   };
-    #
-    # };
+    opts = {
+      hlsearch = false;
+      incsearch = true;
+      number = true;
+      relativenumber = true;
+      mouse = "nv";
+      breakindent = true;
+      undofile = true;
+      ignorecase = true;
+      smartcase = true;
+      updatetime = 50;
+      timeout = true;
+      timeoutlen = 300;
+      completeopt = "menuone,noselect";
+      termguicolors = true;
+      swapfile = false;
+      backup = false;
+      undodir = "vim.fn.stdpath('state') .. '/undodir'";
+      smartindent = true;
+      scrolloff = 8;
+      wrap = false;
+      cursorline = true;
+    };
 
     colorschemes.catppuccin = {
       enable = true;
