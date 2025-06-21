6cd0800b51f5fbc6db8168f5353712ac9cc7c6ca
Rick Henry <rickhenry@rickhenry.dev>
2025-06-21 13:47:37 -0400

    ugh

--- flake.nix
+++ flake.nix
@@ -176,7 +176,7 @@
           };
           modules = [
             ./hosts/nix-minimal/home.nix
-            nixvim.nixosModules.nixvim
+            nixvim.homeManagerModules.nixvim
           ];
           extraSpecialArgs = {
             inherit inputs;
