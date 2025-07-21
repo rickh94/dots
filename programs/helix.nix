59df4d5ca2840bbb410c0dd4b58537b43644b1c3
Rick Henry <rickhenry@rickhenry.dev>
2025-07-20 21:23:32 -0400

    update zellij and helix

--- programs/helix.nix
+++ programs/helix.nix
@@ -2,5 +2,22 @@
   programs.helix.enable = true;
   programs.helix.settings = {
     theme = "catppuccin_macchiato";
+    editor = {
+      line-number = "relative";
+      cursorline = true;
+      lsp = {
+        display-inlay-hints = true;
+      };
+      cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+      indent-guides.render = true;
+      inline-diagnostics = {
+        cursor-line = "error";
+        other-lines = "error";
+      };
+    };
   };
 }
